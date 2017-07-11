28b6f5a10c5387f3172c08fbecfd0eb9e38e5fbf
Phillip Miguel dos Passos <phillipmigueldospassos@gmail.com>
2017-07-11 17:23:19 -0300

    Initial changes

--- simple/main.go
+++ simple/main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
-	"io"
+	"errors"
+	"fmt"
 	"net/http"
 
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 )
 
-func echoHandler(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+type msg struct {
+	msg string
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(echoHandler))
+	http.HandleFunc("/echo", wsHandler)
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
+	}
+
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Origin") != "http://"+r.Host {
+		http.Error(w, "Fora da origem", 403)
+		return
+	}
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Erro ao abrir conexao", http.StatusBadRequest)
+	}
+
+	conn.SetCloseHandler(closeHandler)
+
+	go echo(conn)
+}
+
+func closeHandler(code int, text string) error {
+	fmt.Println("Cliente desconectou")
+	return errors.New("")
+}
+
+func echo(conn *websocket.Conn) {
+	for {
+		m := msg{}
+
+		err := conn.ReadJSON(&m)
+		if err != nil {
+			conn.Close()
+			fmt.Println("Erro ao ler json.", err)
+			return
+		}
+
+		fmt.Printf("Mensagem: %#v\n", m)
+
+		if err = conn.WriteJSON(m); err != nil {
+			fmt.Println(err)
+		}
 	}
 }
